eb7276976ae374b6d5d6ae44c9845f43e8716f22
Lewis Kabui <lewis_maina@yahoo.com>
2015-10-29 22:43:14 +0300

    Utilize OverflowException.

--- office.py
+++ office.py
@@ -1,17 +1,29 @@
 import room
+from overflow import OverflowException
 
 # Office spaces have a default maximum of 6 people
 class Office(room.Room):
 
 	def getMax(self):
+
 		self.max = 6
+
 		return self.max
 
+
 	def addPeople(self, person):
+
 		if isinstance(person, Person):
-			if ( len(self.occupants) <= self.max):
+
+			if ( len(self.occupants) < self.max):
+
 				self.occupants.append(person)
+
 			else:
-				print 'This office space is full!'
+
+				raise OverflowException('This office space is full!')
+
 		else:
-			print 'Office spaces are meant to be allocated to people (Staff & Fellows)'
+
+			raise TypeError('Office spaces are meant to be allocated to people (Staff & Fellows)')
+				
