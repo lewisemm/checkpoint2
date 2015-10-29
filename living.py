41ce4ca4a493da352a524cda2a6086fdcf9f620d
Lewis Kabui <lewis_maina@yahoo.com>
2015-10-29 22:42:25 +0300

    Add constructor to initialize variables in class.

--- living.py
+++ living.py
@@ -1,19 +1,35 @@
 import room
 from overflow import OverflowException
+from fellow import Fellow
 
 # Living spaces have a default maximum of 4 people
 # Only fellows can occupy living spaces
 class Living(room.Room):
 
-	def getMax(self):
+	# constructor
+	def __init__(self):
+		self.occupants = []
 		self.max = 4
+
+
+	def getMax(self):
+
 		return self.max
 
+
+
 	def addFellow(self, fellow):
+		
 		if isinstance(fellow, Fellow):
+
 			if ( len(self.occupants) < self.max):
+
 				self.occupants.append(fellow)
+
 			else:
+
 				raise OverflowException('This living space is full!')
+
 		else:
+
 			raise TypeError('Living spaces are meant for Fellows only!')
