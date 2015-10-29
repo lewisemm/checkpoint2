b97440aad66331b26669d7a023e898aa6d1866a4
Lewis Kabui <lewis_maina@yahoo.com>
2015-10-29 22:41:29 +0300

    Space lines of code.

--- fellow.py
+++ fellow.py
@@ -5,17 +5,24 @@ class Fellow(person.Person):
 
 	# store the name of the living space where the fellow is allocated
 	def setBoardingRoom(self, boardingRoom):
+
 		self.boardingRoom = boardingRoom
+
 
 	# store the name of the office space where the fellow is allocated
 	def setOfficeRoom(self, officeRoom):
+
 		self.officeRoom = officeRoom
+
 
 	# the getters
 	# ------------------
 
 	def getBoardingRoom(self):
+
 		return self.boardingRoom
 
+
 	def getOfficeRoom(self):
+		
 		return self.officeRoom
