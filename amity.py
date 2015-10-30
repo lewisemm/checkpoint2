9452b2e1d7be52614ce1241b852c7e19f170364b
Lewis Kabui <lewis_maina@yahoo.com>
2015-10-30 16:44:26 +0300

    Initialize variables on instantiation of class object. Get class names from allocation.txt file. Assign at random. Put allocate and persist logic in place.

--- amity.py
+++ amity.py
@@ -4,33 +4,39 @@ from staff import Staff
 from fellow import Fellow
 from random import random
 from overflow import OverflowException
+import os
 
 class Amity:
 
 	# constructor
 	def __init__(self):
-		self.living = []
-		self.office = []
+		self.livingRooms = []
+		self.officeRooms = []
 
-	def prePopulate(self, lSpaces, oSpaces):
+	def prePopulate(self):
 		
-		# naming living spaces
-		counter = 1
-		for i in range(lSpaces):
-			liv = Living()
-			liv.setName('Room ' + str(counter))
-			self.living.append(liv)
-			counter += 1
+		# retrieve existing room names at data/allocation.txt
 
-		# naming office spaces
-		counter = 1
-		for j in range(oSpaces):
-			off = Office()
-			off.setName('Office ' + str(counter))
-			self.office.append(off)
-			counter += 1
+		allocation = open('data/allocated.txt')
 
-	
+		while (True):
+			line = allocation.readline()
+			if not line:
+				break
+
+			if (line[0] == '+'):
+				# living spaces start with '+' in the file
+				aLivingRoom = Living()
+				aLivingRoom.setName(line[1::])
+				self.livingRooms.append(aLivingRoom)
+
+			elif (line[0] == '-'):
+				# office spaces start with '-' in the file
+				anOfficeRoom = Office()
+				anOfficeRoom.setName(line[1::])
+				self.officeRooms.append(anOfficeRoom)
+
+		allocation.close()
 
 
 	def allocate(self, person, spaceType):
@@ -62,6 +68,17 @@ class Amity:
 
 					else:
 
+						try:
+							allocated = open('allocated.txt', 'w')
+							allocated.write(self.office[rand].getName() )
+							#allocated.write(self.office[rand].getOccupants().getName() + ', ')
+
+						except IOError:
+							print '*** An IOError occured! *** \n', e, '\n'
+
+						finally:
+							allocated.close()
+						
 						print 'Insert Successful! Allocated to ', self.office[rand].getName()
 
 						break
@@ -71,36 +88,72 @@ class Amity:
 
 
 		def livingSpaceAllocation(person):
+
+			# Assume there's no space available until proven otherwise
 			available = False
 
-			for room in self.living:
-				if len(room.occupants) < room.max:
+			for eachRoom in self.livingRooms:
+
+				if len(eachRoom.occupants) < eachRoom.max:
+
+					# This means there's a space available in one of the rooms
 					available = True
-					break;
+
+					break
 
 			if available:
+
 				# Try allocating this person a living space as long as he encounters OverflowException
-				# because we know there's a space somewhere
+				# because there's a space (guaranteed) somewhere
 				while (True):
 
 					rand = int(random() * 10)
+
+					# random room selection
+					currentRoom = self.livingRooms[rand]
 					
 					try:
-
-						self.living[rand].addFellow(person)
+						
+						currentRoom.addFellow(person)
 
 					except OverflowException:
 
+						# loop afresh to find another random room
 						continue
 
 					# when attempting to allocate staff a living space
 					except TypeError:
 
+						# loop afresh to find another random room
 						continue
 
 					else:
 
-						print 'Insert Successful! Allocated to ', self.living[rand].getName()
+						# if person has found a room, persist this info.
+						try:
+
+							allocated = open('data/allocated.txt', 'r+')
+							temp = open('data/temp.txt', 'w')
+
+							# get the starting index where the room's name first appears
+							for lines in allocated.readlines():
+
+								temp.write(lines)
+
+								if lines[1::] == currentRoom.getName():
+									temp.write('\n\t' + person.getName() + '\n')
+
+							os.remove('data/allocated.txt')
+							os.rename('data/temp.txt', 'data/allocated.txt')
+									
+						except Exception as e:
+							print '*** An Exception occured! *** \n', e
+
+						finally:
+							allocated.close()
+							temp.close()
+
+						print 'Insert Successful! Allocated to ', currentRoom.getName()
 
 						break
 
@@ -114,9 +167,7 @@ class Amity:
 		elif isinstance(person, Fellow):
 
 			if (spaceType == 'Office'):
-
 				officeAllocation(person)
 
 			elif (spaceType == 'Living'):
-
 				livingSpaceAllocation(person)
