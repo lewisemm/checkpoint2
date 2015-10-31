76f60daf5b01bc5c7b1a4a568b2ef87a2f506fcb
Lewis Kabui <lewis_maina@yahoo.com>
2015-10-31 11:02:15 +0300

    Create constructor. Add getters for max and occupants variables.

--- office.py
+++ office.py
@@ -1,12 +1,16 @@
 import room
 from overflow import OverflowException
+from person import Person
 
 # Office spaces have a default maximum of 6 people
 class Office(room.Room):
 
-	def getMax(self):
-
+	# constructor
+	def __init__(self):
+		self.occupants = []
 		self.max = 6
+
+	def getMax(self):
 
 		return self.max
 
@@ -26,4 +30,7 @@ class Office(room.Room):
 		else:
 
 			raise TypeError('Office spaces are meant to be allocated to people (Staff & Fellows)')
+
+	def getOccupants(self):
+		return self.occupants
 				
