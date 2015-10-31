087fc13e65a17fb17440bda5eb182138a035571e
Lewis Kabui <lewis_maina@yahoo.com>
2015-10-31 11:03:36 +0300

    Create file to store cross functional logic.

---
+++ utilities.py
@@ -0,0 +1,14 @@
+import os
+from fellow import Fellow
+
+# definitions for functionalities across the app
+def clearScreen():
+	ans = os.system('cls')
+
+	if (ans != 0):
+		os.system('clear')
+
+
+
+	
+
