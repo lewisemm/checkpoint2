e53e3019d72589f03fea1e7f5b5089dacbf77361
Lewis Kabui <lewis_maina@yahoo.com>
2015-11-01 10:19:45 +0300

    Add docstrings to methods. Include mechanism to separate Fellows from Staff in file. Randomize space allocation in dynamic manner instead of constant 10 rooms.

--- amity.py
+++ amity.py
@@ -36,59 +36,118 @@ class Amity:
 				anOfficeRoom.setName(line[1::])
 				self.officeRooms.append(anOfficeRoom)
 
-			elif (line[0] == '\t'):
-				# insert these people at the lastIndex
+			elif (line[0] == '\t' and line[1] != '>' and line[1] != '#'):
+				# This is a fellow in a living space
+
+				# insert the current fellow at the lastIndex
 				lastIndex = len(self.livingRooms) - 1
 
 				# Create a fellow object from the name on the file
 				fellow = Fellow(line[1::])
 				self.livingRooms[lastIndex].addFellow(fellow)
 
+			elif (line[0] == '\t' and line[1] == '#'):
+				# This is a fellow in an office space
+
+				# insert these people at the lastIndex
+				lastIndex = len(self.officeRooms) - 1
+
+				# Create a fellow object from the name on the file
+				fellow = Fellow(line[2::])
+				self.officeRooms[lastIndex].addPeople(fellow)
+
+			elif (line[0] == '\t' and line[1] == '>'):
+				# This is a staff member in an office space
+
+				# insert these people at the lastIndex
+				lastIndex = len(self.officeRooms) - 1
+
+				# Create a staff object from the name on the file
+				staff = Staff(line[2::])
+				self.officeRooms[lastIndex].addPeople(staff)
+
 
 		allocation.close()
 
 
 	def allocate(self, person, spaceType):
+		""" Allocates Andelans to an office space at random. """
 
 		def officeAllocation(person):
+			""" Allocates Andelans to an office. """
+
 			# first check if there's room in any office
 			available = False
 
 			# find at least one available space to enable allocation
-			for ofisi in self.office:
-				if len(ofisi.occupants) < ofisi.max:
+			for ofisi in self.officeRooms:
+
+				if len(ofisi.getOccupants()) < ofisi.max:
+
 					available = True
+
 					break;
 
 			if available:
 				# Try allocating this person an office as long as he encounters OverflowException
 				# because we know from variable 'available' that there's a space somewhere
+
+				officeCount = len(self.officeRooms)
+
 				while (True):
 
-					rand = int(random() * 10)
+					rand = int(random() * officeCount)
+
+					currentOffice = self.officeRooms[rand]
 				
 					try:
 
-						self.office[rand].addPeople(person)
+						currentOffice.addPeople(person)
 
 					except OverflowException:
 
+						print 'Cannot allocate to ', currentOffice.getName(), ' ... moving on.'
+						# loop afresh and search for another room
 						continue
 
 					else:
 
-						try:
-							allocated = open('allocated.txt', 'w')
-							allocated.write(self.office[rand].getName() )
-							#allocated.write(self.office[rand].getOccupants().getName() + ', ')
+						edits = ''
 
-						except IOError:
-							print '*** An IOError occured! *** \n', e, '\n'
+						try:
+
+							allocated = open('data/allocated.txt', 'r+')
+
+							for line in allocated.readlines():
+
+								edits += line
+
+								if (line[1::] == currentOffice.getName()):
+
+									if isinstance(person, Fellow):
+
+										edits += '\n\t#' + person.getName() + '\n'
+
+									elif isinstance(person, Staff):
+
+										edits += '\n\t>' + person.getName() + '\n'	
+									
+
+
+							# reposition pointer back to beginning to 
+							# overwrite old data with this modified version
+							allocated.seek(0)
+
+							# write the modified version
+							allocated.write(edits)
+
+						except Exception as e:
+							print '*** An Exception occured! *** \n', e, '\n'
 
 						finally:
 							allocated.close()
 						
-						print 'Insert Successful! Allocated to ', self.office[rand].getName()
+						print 'Insert Successful! Allocated to office space ', currentOffice.getName()
 
 						break
 
@@ -97,13 +156,14 @@ class Amity:
 
 
 		def livingSpaceAllocation(person):
+			""" Allocates fellows to a living space at random. """
 
 			# Assume there's no space available until proven otherwise
 			available = False
 
 			for eachRoom in self.livingRooms:
 
-				if len(eachRoom.occupants) < eachRoom.max:
+				if len(eachRoom.getOccupants()) < eachRoom.max:
 
 					# This means there's a space available in one of the rooms
 					available = True
@@ -114,9 +174,12 @@ class Amity:
 
 				# Try allocating this person a living space as long as he encounters OverflowException
 				# because there's a space (guaranteed) somewhere
+
+				roomCount = len(self.livingRooms)
+
 				while (True):
 
-					rand = int(random() * 10)
+					rand = int(random() * roomCount)
 
 					# random room selection
 					currentRoom = self.livingRooms[rand]
@@ -126,7 +189,7 @@ class Amity:
 						currentRoom.addFellow(person)
 
 					except OverflowException:
-
+						print 'Cannot allocate to ', currentRoom.getName(), ' ... moving on.'
 						# loop afresh to find another random room
 						continue
 
@@ -162,7 +225,7 @@ class Amity:
 							allocated.close()
 							temp.close()
 
-						print 'Insert Successful! Allocated to ', currentRoom.getName()
+						print 'Insert Successful! Allocated to living space ', currentRoom.getName()
 
 						break
 
