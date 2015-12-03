a0a127dad92b4f014c6e9eb77b3ec70288c3e9cd
Lewis Kabui <lewis.maina@andela.com>
2015-12-03 17:11:13 +0300

    Dirty

--- checkpoint2/api.py
+++ checkpoint2/api.py
@@ -3,9 +3,11 @@ import os
 from flask import Flask, render_template, Markup, request, redirect, url_for, flash
 from flask_restful import Resource, Api, reqparse
 from flask.json import jsonify
-from flask.ext.login import LoginManager, login_required, login_user, logout_user
+from flask.ext.login import LoginManager, login_required, login_user, logout_user, current_user
 
-from sqlalchemy import create_engine, MetaData
+from flask_httpauth import HTTPBasicAuth
+
+from sqlalchemy import create_engine, MetaData, desc, func
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.serializer import loads, dumps
 
@@ -16,6 +18,8 @@ import models
 app = Flask(__name__)
 api = Api(app)
 app.config.update(SECRET_KEY=os.environ['SECRET_KEY'])
+
+auth = HTTPBasicAuth()
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -30,9 +34,11 @@ manager = session()
 def load_user(user_id):
 	return manager.query(models.User).filter_by(id=user_id).first()
 
+@auth.verify_password
 def verify_password(username_or_token, password):
 
 	user = models.User.verify_auth_token(username_or_token)
+	ipdb.set_trace()
 
 	if not user:
 		user = manager.query(models.User).filter_by(username = username_or_token).first()
@@ -63,7 +69,6 @@ def registration():
 
 			user = models.User(username=username)
 			user.hash_password(password)
-			print 'B* please!!!'
 			manager.add(user)
 			manager.commit()
 
@@ -71,25 +76,16 @@ def registration():
 		except Exception as e:
 			return render_template('register.html', error=e)
 
+
 class BucketList(Resource):
 	@login_required
 	def post(self):
 		try:
-			# parser = reqparse.RequestParser()
-			# parser.add_argument('name', type=str, help='Name of the bucket list')
-			# parser.add_argument('created_by', type=str, help='Name of the user who created bucket list')
-
-			# args = parser.parse_args()
-			# name = args['name']
-			# created_by = current_user.username
-			json_data = request.json
-
-			for bucket in json_data:
-				name = bucket['name']
-				created_by = request.user
+			json_data = request.get_json()
+			for bucketlist in json_data:
+				name = bucketlist['name']
+				created_by = current_user.username
 				bucket = models.BucketList(name=name, created_by=created_by)
-				ipdb.set_trace()
-				manager = session()
 				manager.add(bucket)
 				manager.commit()
 			
@@ -98,43 +94,61 @@ class BucketList(Resource):
 		except Exception as e:
 			return {'error': str(e)}
 
-	@login_required
-	def get(self):
-		result = manager.query(models.BucketList).all()
+	@auth.login_required
+	def get(self, limit=20):
+
+		result = manager.query(models.BucketList).order_by(desc(models.BucketList.date_created)).limit(limit)
+		
 		res_json = []
 		for bucket in result:
 			current_bucket = {}
+			current_items = {}
 			current_bucket['id'] = bucket.buck_id
 			current_bucket['name'] = bucket.name
+
+			item_in_bucket = manager.query(models.Item).filter(models.Item.bucket_id==bucket.buck_id).order_by(desc(models.Item.date_created)).all()
 			current_bucket['items'] = []
+
+			for item in item_in_bucket:
+				current_items['id'] = item.item_id
+				current_items['name'] = item.name
+				current_items['date_created'] = str(item.date_created)
+				current_items['date_modified'] = str(item.date_modified)
+				current_items['done'] = item.done
+
+			if current_items:
+				current_bucket['items'].append(current_items)
+				
 			current_bucket['date_created'] = str(bucket.date_created)
 			current_bucket['date_modified'] = str(bucket.date_modified)
 			current_bucket['created_by'] = bucket.created_by
 
 			res_json.append(current_bucket)
 
-		return ({'bucketlist': res_json})
+		return jsonify({'bucketlists': res_json})
 
 api.add_resource(BucketList, '/bucketlists/')
+
 
 class BucketListID(Resource):
 	@login_required
 	def put(self, id):
 		try:
-			parser = reqparse.RequestParser()
-			parser.add_argument('name', type=str, help='Name of the bucket list')
-			parser.add_argument('created_by', type=str, help='Name of the user who created bucket list')
+			json_data = request.get_json()
 
-			args = parser.parse_args()
-			name = args['name']
-			created_by = args['created_by']
+			bucketlist = manager.query(models.BucketList).filter_by(buck_id=id).first()
+			
+			if bucketlist:
+				bucketlist.name = json_data['name']
+				bucketlist.date_modified = func.now()
+			
+				manager.add(bucketlist)
+				manager.commit()
 
-			bucket = models.BucketList(name=name, created_by=created_by)
-			manager = session()
-			manager.add(bucket)
-			manager.commit()
+				return {'message': 'Bucketlist updated successfully'}
+			else:
+				return {'message': 'That bucket list id doesnt exist'}
 
-			return {'message': 'successful post'}
 		except Exception as e:
 			return {'error': str(e)}
 
@@ -157,33 +171,69 @@ class BucketListID(Resource):
 	
 	@login_required
 	def delete(self, id):
-		pass
+		bucketlist = manager.query(models.BucketList).filter_by(buck_id=id).first()
+		manager.delete(bucketlist)
+		manager.commit()
+
+		return {'message': 'bucketlist of id ' + str(id) + ' has been deleted'}
 
 api.add_resource(BucketListID, '/bucketlists/<int:id>')
+
 
 class BucketListItems(Resource):
 	@login_required
 	def post(self, id):
-		pass
 
+		# check if bucketlist exists
+		bucketlist = manager.query(models.BucketList).filter_by(buck_id=id).first()
+
+		if bucketlist:
+			json_data = request.get_json()
+			name = json_data['name']
+			item = models.Item(name=name, bucket_id=bucketlist.buck_id)
+			manager.add(item)
+			manager.commit()
+
+			return ({'message': 'Item added to bucketlist id ' + str(id)})
+		else:
+			return ({'bucketlist': 'Bucket of id ' + str(id) + ' doesnt exist'})
 
 api.add_resource(BucketListItems, '/bucketlists/<int:id>/items/')
 
+
 class BucketListItemsID(Resource):
 	@login_required
-	def put(self, id):
-		pass
+	def put(self, id, item_id):
+
+		# check if bucketlist exists
+		bucketlist = manager.query(models.BucketList).filter_by(buck_id=id).first()
+
+		if bucketlist:
+
+			json_data = request.get_json()
+
+			item = manager.query(models.Item).filter_by(item_id=item_id).first()
+			item.name = json_data['name']
+			item.date_modified = func.now()
+			item.done = json_data['done']
+
+			manager.add(item)
+			manager.commit()
+
+			return ({'bucketlist': 'Item of id ' + str(item_id) + 'from bucket of id ' + str(id) + ' has been updated'})
+		else:
+			return ({'bucketlist': 'Bucket of id ' + str(id) + ' doesnt exist'})
+
 
 	@login_required
-	def delete(self, id):
+	def delete(self, id, item_id):
 		pass
 
-api.add_resource(BucketListItemsID, '/bucketlists/<int:id>/items/<int:items_id>')
+api.add_resource(BucketListItemsID, '/bucketlists/<int:id>/items/<int:item_id>')
 
 @app.route('/login/token', methods=['GET'])
-@login_required
 def tokenize():
-	token = g.user.generate_auth_token()
+	token = current_user.generate_auth_token()
 	return jsonify({'token': token.decode('ascii')})
 
 
@@ -197,21 +247,22 @@ def login():
 		return render_template('login.html', error=None)
 	
 	elif request.method == 'POST':
-		json_data = request.get_json()
-		username = str(json_data['username'])
-		password = str(json_data['password'])
+
+		username = request.form.get('username')
+		password = request.form.get('password')
 
 		user = manager.query(models.User).filter_by(username=username).first()
+
 		if not user:
 			return render_template('login.html', error='That username does not exist!')
 		elif user.verify_password(password):
-			login_user(user)
-			
 
-        	return jsonify({'message': 'Logged in successfully.'})
-    	return render_template('login.html', error='Neither GET nor POST')
+			if user.verify_password(password):
+				login_user(user)
+				return redirect(url_for('tokenize'))
+			else:
+				return render_template('login.html', error='Invalid password!')
 		
-			
 @app.route('/auth/logout', methods=['GET'])
 @login_required
 def logout():
--- checkpoint2/models.py
+++ checkpoint2/models.py
@@ -6,6 +6,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
+from flask import current_app
 from flask.ext.login import UserMixin
 
 from passlib.apps import custom_app_context as pwd_context
@@ -51,12 +52,12 @@ class User(Base, UserMixin):
 		return pwd_context.verify(password, self.password_hash)
 
 	def generate_auth_token(self, expiration=600):
-		s = Serialization(app.config['SECRET_KEY'], expires_in = expiration)
+		s = Serializer(current_app.config['SECRET_KEY'], expires_in = expiration)
 		return s.dumps({'id':self.id})
 
 	@staticmethod
 	def verify_auth_token(token):
-		s = Serializer(app.config['SECRET_KEY'])
+		s = Serializer(current_app.config['SECRET_KEY'])
 		try:
 			data = s.loads(token)
 		except SignatureExpired:
